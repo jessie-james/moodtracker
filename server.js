bc4d4df790c838c6bb633a485943c0330c1301b1
Jessie James <jess.jamestwoo11@gmail.com>
2019-09-27 14:25:06 -0600

    make it better

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const app = express();
 const mongoose = require("mongoose");
 const expressJwt = require("express-jwt");
 const PORT = process.env.PORT || 3742;
+const path = require("path")
 
 require("dotenv").config();
 
 app.use("/", express.json())
 app.use("/api", expressJwt({ secret: process.env.SECRET }));
+app.use(express.static(path.join(__dirname, "client", "build")))
 
+// ...
+// Right before your app.listen(), add this:
 
 app.use("/auth", require("./routes/auth"));
 app.use("/api/survey", require("./routes/moodsurvey"));
 app.use("/api/client", require("./routes/client"));
 
-mongoose.connect("mongodb://localhost:27017/moodtracker",
+mongoose.connect(process.env.MONGODB_URI||"mongodb://localhost:27017/moodtracker",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
@@ -31,6 +35,9 @@ app.use((err, req, res, next) => {
         res.status(err.status)
     }
     return res.send({ message: err.message });
+});
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
