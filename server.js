4a1fa043d746a8137136a718d62f10bc0e8e839c
Jessie James <jess.jamestwoo11@gmail.com>
2019-09-28 12:06:57 -0600

    new deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const expressJwt = require("express-jwt");
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3472;
 const path = require("path")
 
 require("dotenv").config();
